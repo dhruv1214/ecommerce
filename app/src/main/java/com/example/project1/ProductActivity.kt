9e3dab9d69bc22b83c6e6b5bc59878807af7bb95
Dhruv <dhruv2146desai@gmail.com>
2023-07-09 14:49:06 -0400

    new files added

--- app/src/main/java/com/example/project1/ProductActivity.kt
+++ app/src/main/java/com/example/project1/ProductActivity.kt
@@ -1,32 +1,33 @@
 package com.example.project1
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.firebase.ui.database.FirebaseRecyclerOptions
 import com.google.firebase.database.FirebaseDatabase
 
-class ProductActivity : AppCompatActivity() {
 
-    private var rView: RecyclerView? = null;
-    private var adapter: ProductsAdapter? = null
+    class ProductActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.product)
+        private var rView: RecyclerView? = null;
+        private var adapter: ProductsAdapter? = null
 
-        val query = FirebaseDatabase.getInstance().reference.child("products")
-        val options = FirebaseRecyclerOptions.Builder<Products>().setQuery(query, Products::class.java).build()
-        adapter = ProductsAdapter(options)
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.product)
 
-        rView = findViewById(R.id.rView)
-        rView?.layoutManager = LinearLayoutManager(this)
-        rView?.adapter = adapter;
+            val query = FirebaseDatabase.getInstance().reference.child("Products")
+            val options = FirebaseRecyclerOptions.Builder<Products>().setQuery(query, Products::class.java).build()
+            adapter = ProductsAdapter(options)
+
+            rView = findViewById(R.id.rView)
+            rView?.layoutManager = LinearLayoutManager(this)
+            rView?.adapter = adapter;
+        }
+
+        override fun onStart() {
+            super.onStart()
+            adapter?.startListening()
+        }
     }
-
-    override fun onStart() {
-        super.onStart()
-        adapter?.startListening()
-    }
-}
